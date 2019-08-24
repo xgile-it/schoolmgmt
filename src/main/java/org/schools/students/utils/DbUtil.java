cda6360972d67ad2a56838e4aaf92d9167fef9bb
NB307888 <himanshus@nedbank.co.za>
2019-08-24 08:43:19 +0200

    Default Changelist

--- src/main/java/org/schools/students/utils/DbUtil.java
+++ src/main/java/org/schools/students/utils/DbUtil.java
@@ -3,9 +3,11 @@ package org.schools.students.utils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Properties;
 import java.io.*;
 public class DbUtil {
@@ -16,41 +18,54 @@ public class DbUtil {
             return connection;
         else {
             try {
-            	Properties prop = new Properties();
-                System.out.println("File Loaded");
+                Properties prop = new Properties();
+                // System.out.println("File Loaded");
 
-                File file = new File(
+              /*  File file = new File(
                         getClass().getClassLoader().getResource("db.properties").getFile()
-                );
+                );*/
 
-                System.out.println("File Loaded"+file.getName());
+                // System.out.println("File Loaded"+file.getName());
 
-                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
-                prop.load(inputStream);
-                System.out.println("File Loaded");
-                String driver = prop.getProperty("driver");
-                String url = prop.getProperty("url");
-                String user = prop.getProperty("user");
-                String password = prop.getProperty("password");
+                // InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
+                //    prop.load(inputStream);
+                // System.out.println("File Loaded");
+                String driver = "com.mysql.jdbc.Driver";
+                String url = "jdbc:mysql://localhost:3306/schoolmgmt";
+                String user = "root";
+                String password = "root";
                 Class.forName(driver);
                 connection = DriverManager.getConnection(url, user, password);
-            } catch (ClassNotFoundException e) {
+                return connection;
+            } catch (Exception e) {
                 e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return connection;
-        }
 
+                return connection;
+            }
+
+        }
     }
 
-
-    public static void main(String[]args){
-        DbUtil dbu= new DbUtil();
-        dbu.getConnection();
+        public static void main(String[]args){
+            DbUtil dbu = new DbUtil();
+            Connection connection= dbu.getConnection();
+            System.out.println("DB Connection Created");
+            String sql = "INSERT INTO Users (username, password, fullname, email) VALUES (?, ?, ?, ?)";
+try {
+    PreparedStatement statement = connection.prepareStatement(sql);
+    statement.setString(1, "bill");
+    statement.setString(2, "secretpass");
+    statement.setString(3, "Bill Gates");
+    statement.setString(4, "bill.gates@microsoft.com");
+   int rowsInserted = statement.executeUpdate();
+    if (rowsInserted > 0) {
+        System.out.println("A new user was inserted successfully!");
     }
 }
+            catch(SQLException ex)
+    {
+             ex.printStackTrace();
+    }
+}
+        }
+
